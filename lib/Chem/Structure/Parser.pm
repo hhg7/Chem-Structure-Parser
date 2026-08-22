d3ba366007fc16aa06d26fc56e35fa78ea7515d8
hhg7 <dec986@gmail.com>
2026-08-22 07:37:00 -0500

    Update generated docs

--- lib/Chem/Structure/Parser.pm
+++ lib/Chem/Structure/Parser.pm
@@ -2685,6 +2685,10 @@ David E. Condon L<mailto:dec986@gmail.com>
 
 =head1 Changes
 
+=head2 0.02 2026-08-22 CDT
+
+Fix for Windows: https://www.cpantesters.org/cpan/report/960c8de0-8001-1014-8897-e6a949c55ebf
+
 =head2 0.01 2026-08-21 CDT
 
 initial version: reads PDB into a hash of hashes, single-letter
